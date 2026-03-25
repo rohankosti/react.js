ca678fe6bc0c9f980a8c333f0972c3ad3c3a1b6d
rohankosti <kostirohan186@gmail.com>
2026-03-25 19:27:19 +0530

    update

--- react-rohan/src/components/Form.jsx
+++ react-rohan/src/components/Form.jsx
@@ -1,20 +1,36 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function form(props) {
-    const [text, settext] =useState("Enter the text");
+  const [text, settext] = useState("Enter the text");
 
-    function click() {
-        console.log("sd");
-        
-    }
+  function click() {
+    // console.log(text);
+    let Uc = text.toUpperCase();
 
-    return(
-        <div className="mb-3">
-  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" value={text} id="exampleFormControlTextarea1" rows="5"></textarea>
-  <button className='btn btn-primary mt-3' onClick={click}>Convert to Upercase</button>
-</div>
-    
-)
+    settext(Uc);
+  }
+  function change(event) {
+    settext(event.target.value);
+  }
+
+  return (
+    <div className="mb-3">
+      <label htmlFor="exampleFormControlTextarea1" className="form-label">
+        Example textarea
+      </label>
+      <textarea
+        className="form-control"
+        onChange={change}
+        value={text}
+        id="exampleFormControlTextarea1"
+        rows="5"
+      ></textarea>
+      <button className="btn btn-primary mt-3" onClick={click}>
+        Convert to Upercase
+      </button>
+      <h1 className="my-3">Preview</h1>
+      <div>{text}</div>
+    </div>
+  );
 }
-export default form
+export default form;
